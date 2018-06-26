bf6f651a082e43521b614ee9ab024c6420a36b38
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2018-06-26 16:35:16 +0100

    Added HYDRO evaluations

---
+++ DataHelper.py
@@ -0,0 +1,29 @@
+
+import config, requests
+from urllib.parse import urlparse
+from os.path import splitext, basename
+import os
+
+class DataDownloader():
+    def __init__(self):
+        self.cwd = os.getcwd()
+
+    def downloadFiles(self, urls):
+        for url in urls: 
+            disassembled = urlparse(url)
+            filename = basename(disassembled.path)
+            ext = os.path.splitext(disassembled.path)[1]
+
+            workingdirectory = os.path.join(self.cwd,config.settings['workingdirectory'])
+            if not os.path.exists(workingdirectory):
+                os.mkdir(workingdirectory)
+            local_filename = os.path.join(workingdirectory, filename)
+            if not os.path.exists(local_filename):
+                print("Downloading from %s..." % url)
+                r = requests.get(url, stream=True)
+                with open(local_filename, 'wb') as f:
+                    for chunk in r.iter_content(chunk_size=1024): 
+                        if chunk: # filter out keep-alive new chunks
+                            f.write(chunk)
+                            #f.flush() commented by recommendation from J.F.Sebastian
+            return local_filename
--- Raster-Evaluations-Processor.py
+++ Raster-Evaluations-Processor.py
@@ -12,32 +12,7 @@ from rasterio import crs
 from rasterio.tools.mask import mask
 from pysal.esda.mapclassify import Natural_Breaks as nb
 import config, requests
-from urllib.parse import urlparse
-from os.path import splitext, basename
-
-class DataDownloader():
-    def __init__(self):
-        self.cwd = os.getcwd()
-
-    def downloadFiles(self, urls):
-        for url in urls: 
-            disassembled = urlparse(url)
-            filename = basename(disassembled.path)
-            ext = os.path.splitext(disassembled.path)[1]
-
-            workingdirectory = os.path.join(self.cwd,config.settings['workingdirectory'])
-            if not os.path.exists(workingdirectory):
-                os.mkdir(workingdirectory)
-            local_filename = os.path.join(workingdirectory, filename)
-            if not os.path.exists(local_filename):
-                print("Downloading from %s..." % url)
-                r = requests.get(url, stream=True)
-                with open(local_filename, 'wb') as f:
-                    for chunk in r.iter_content(chunk_size=1024): 
-                        if chunk: # filter out keep-alive new chunks
-                            f.write(chunk)
-                            #f.flush() commented by recommendation from J.F.Sebastian
-            return local_filename
+import DataHelper
 
 class EvaluationsProcessor():
     def __init__(self):
@@ -65,7 +40,7 @@ class EvaluationsProcessor():
     def computeTransportNaturalBreaks(self, rawtransfile):
         print("Computing Natural breaks on Transport..")
 
-        myDataDownloader = DataDownloader()
+        myDataDownloader = DataHelper.DataDownloader()
         localfile = myDataDownloader.downloadFiles([config.settings['aoi']])
 
         with fiona.open(localfile, "r") as aoi:
@@ -219,7 +194,7 @@ class EvaluationsProcessor():
 
     def cropSlopeAndNDVI(self):
         # Open the 
-        myDataDownloader = DataDownloader()
+        myDataDownloader = DataHelper.DataDownloader()
         localfile = myDataDownloader.downloadFiles([config.settings['aoi']])
 
         with fiona.open(localfile, "r") as aoi:
