437787b3dd52509510762bc0b0594341ff83aec4
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2018-07-20 12:09:46 +0100

    Update HYDRO-Evaluations-Processor.py

--- HYDRO-Evaluations-Processor.py
+++ HYDRO-Evaluations-Processor.py
@@ -9,7 +9,6 @@ cwd = os.getcwd()
 myDataDownloader = DataHelper.DataDownloader()
 aoipath = myDataDownloader.downloadFiles([config.settings['aoi']])
 
-
 riverpath = os.path.join(cwd, config.settings['workingdirectory'], config.settings['rivers'])
 watershedpath = os.path.join(cwd, config.settings['workingdirectory'], config.settings['watersheds'])
 outputgeojson= os.path.join(cwd,config.settings['outputdirectory'],'evals/HYDRO/HYDRO.geojson')
@@ -17,15 +16,18 @@ river = geopandas.GeoDataFrame.from_file(riverpath) # or geojson etc
 aoi = geopandas.GeoDataFrame.from_file(aoipath) 
 watershed = geopandas.GeoDataFrame.from_file(watershedpath)
 
-watershed_with_rivers = sjoin(watershed,river, how='inner', op='intersects')
+watershed['area'] = watershed.apply(lambda row: (row['geometry'].area), axis=1) 
+river['length'] = river.apply(lambda row: (row['geometry'].length), axis=1)
 
+watershed_with_rivers = sjoin(watershed,river, how='inner', op='intersects')
 
 watershedSumbyRiver = watershed_with_rivers.groupby([
     "HYBAS_ID",
-]).agg(dict(dimenson="sum")).reset_index()
+]).agg(dict(length="sum")).reset_index()
 
-watershed['riverlength'] = watershed.HYBAS_ID.map(watershedSumbyRiver.set_index('HYBAS_ID')['dimenson'].to_dict())
-
+watershed['riverlength'] = watershed.HYBAS_ID.map(watershedSumbyRiver.set_index('HYBAS_ID')['length'].to_dict())
+# replace NaN with a very small number 
+watershed['riverlength'].fillna(1, inplace=True)
 watershed['density'] = watershed.apply(lambda row: (row.area / row.riverlength), axis=1) 
 
 ii = watershed.as_matrix(['density'])
