c1f54656af4aceb0ecd3e57d4e311a0e9d94a4ca
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2018-07-20 11:34:04 +0100

    Update HYDRO-Evaluations-Processor.py

--- HYDRO-Evaluations-Processor.py
+++ HYDRO-Evaluations-Processor.py
@@ -1,17 +1,43 @@
 import geopandas
 from geopandas.tools import sjoin
-
+from pysal.esda.mapclassify import Natural_Breaks as nb
+import numpy as np
+import DataHelper
 import os, config
 cwd = os.getcwd()
 
+myDataDownloader = DataHelper.DataDownloader()
+aoipath = myDataDownloader.downloadFiles([config.settings['aoi']])
+
+
 riverpath = os.path.join(cwd, config.settings['workingdirectory'], config.settings['rivers'])
-watershedpath = os.path.join(cwd, config.settings['workingdirectory'], config.settings['rivers'])
+watershedpath = os.path.join(cwd, config.settings['workingdirectory'], config.settings['watersheds'])
+outputgeojson= os.path.join(cwd,config.settings['outputdirectory'],'evals/HYDRO/HYDRO.geojson')
+river = geopandas.GeoDataFrame.from_file(riverpath) # or geojson etc
+aoi = geopandas.GeoDataFrame.from_file(aoipath) 
+watershed = geopandas.GeoDataFrame.from_file(watershedpath)
 
-point = geopandas.GeoDataFrame.from_file(riverpath) # or geojson etc
-poly = geopandas.GeoDataFrame.from_file(watershedpath)
-pointInPolys = sjoin(point, poly, how='inner', op='intersects')
-county_counts = pointInPolys.groupby([
-    "a_ARCID_left",
-]).agg(dict(length_right="count")).reset_index()
+watershed_with_rivers = sjoin(watershed,river, how='inner', op='intersects')
 
-print county_counts
+
+watershedSumbyRiver = watershed_with_rivers.groupby([
+    "HYBAS_ID",
+]).agg(dict(dimenson="sum")).reset_index()
+
+watershed['riverlength'] = watershed.HYBAS_ID.map(watershedSumbyRiver.set_index('HYBAS_ID')['dimenson'].to_dict())
+
+watershed['density'] = watershed.apply(lambda row: (row.area / row.riverlength), axis=1) 
+
+ii = watershed.as_matrix(['density'])
+breaks = nb(ii.ravel(),k=3,initial=1)
+
+digitizedbins = np.digitize(watershed.density, bins = breaks.bins.tolist())
+watershed['areatype']= digitizedbins
+
+watershed['areatype'] = watershed['areatype'].map({3:'green',2:'green2',1:'green2',0:'green3'})
+
+inter = geopandas.overlay(watershed, aoi, how='intersection')
+
+with open(outputgeojson, 'w') as f:
+    f.write(inter.to_json())
+    
